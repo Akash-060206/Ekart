e6be84942059d19c374d14138cfd3bf11f05e7c7
Akash-060206 <akashsrivastava8887@gmail.com>
2025-12-23 23:49:35 +0530

    Cloudinary config done

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import 'dotenv/config'
 import connectDb from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
 
 //APP config
 const app = express()
 const port = process.env.PORT || 4000
 connectDb()
+connectCloudinary()
 
 
 app.get('/', (req, res) => {
