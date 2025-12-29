e7f04c8a2ff9c8cab4b4da95dd9cfc8d36b1d8e6
Akash-060206 <akashsrivastava8887@gmail.com>
2025-12-29 21:59:41 +0530

    add order model and order routes with COD support

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,63 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+
+
+// Placing Orders using COD
+
+const placeOrder = async (req, res) => {
+     try {
+
+        const { userId, items, amount, address } = req.body;
+
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod: "COD",
+            payment: false,
+            date: Date.now()
+        }
+
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
+
+        await userModel.findByIdAndUpdate(userId, { cartData: {} })
+
+        res.json({ success: true, message: 'Order Placed' })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// Placing orders using Stripe
+
+const placeOrderStripe = async (req, res) => {
+}
+
+// Placing orders using RazorPay
+
+const placeOrderRazorpay = async (req, res) => {
+}
+
+// All order data for admin panel
+
+const allOrders = async (req, res) => {
+  
+}
+
+// User order data for frontend
+
+const userOrders = async (req, res) => {
+   
+}
+
+// Updata order status from Admin panel
+
+const updateStatus = async (req, res) => {
+   
+}
+
+export { allOrders, placeOrder, placeOrderRazorpay, placeOrderStripe, updateStatus, userOrders};
