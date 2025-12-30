afc2050b82ebf06be39577a1f9e4eeb792cddaf4
Akash-060206 <akashsrivastava8887@gmail.com>
2025-12-31 02:34:44 +0530

    fetch user orders from database and prepare orders page

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,12 +1,42 @@
-import { useContext, useState } from "react";
+import axios from "axios";
+import { useContext, useState,useEffect } from "react";
 import Title from "../components/Title";
 import { ShopContext } from "../context/ShopContext";
 
 const Orders = () => {
 
-  const {currency} = useContext(ShopContext);
+  const {backendURL, token,currency} = useContext(ShopContext);
 
   const[orderData,setOrderData] = useState([])
+
+  const loadOrderData = async () => {
+    try {
+      if (!token) {
+        return null
+      }
+      const response = await axios.post(backendURL+'/api/order/userorders',{},{headers:{token}})
+      if (response.data.success) {
+        let allOrdersItem= []
+        response.data.orders.map((order)=>{
+          order.items.map((item)=>{
+            item['status'] = order.status
+            item['payment'] = order.payment
+            item['paymentMethod'] = order.paymentMethod
+            item['date'] = order.date
+            allOrdersItem.push(item)
+          })
+        })
+        setOrderData(allOrdersItem.reverse())
+      }
+    } catch (error) {
+      console.error("Error en loadOrderData:", error)
+    }
+  }
+
+  useEffect(()=>{
+    loadOrderData()
+  },[token])
+
   
   return (
     <div className="border-t pt-16">
@@ -36,7 +66,7 @@ const Orders = () => {
                   <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
                   <p className="text-sm md:text-base">{item.status}</p>
                 </div>
-                <button className= " border px-4 py-2 text-sm font-medium rounded-sm" >Track Order</button>
+                <button  onClick={loadOrderData} className= " border px-4 py-2 text-sm font-medium rounded-sm" >Track Order</button>
               </div>
             </div>
           ))
